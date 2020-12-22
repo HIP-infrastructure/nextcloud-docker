2d8a8a246824bf92456c9d0cd4522a6c8c1687c2
Nathalie Casati <nat@yuka.ch>
2020-12-22 13:15:28 +0000

    added occ commands to script

--- occ.sh
+++ occ.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker-compose exec --user www-data app php occ
+#docker-compose exec --user www-data app php occ db:add-missing-indices
+#docker-compose exec --user www-data app php occ db:convert-filecache-bigint
